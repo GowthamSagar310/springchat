b011b73a01c6ba9b43f5fb350c9b7149ebc5eedf
GowthamSagar310 <gowthamsagartummeda@gmail.com>
2025-01-16 12:27:03 +0530

    HomeController to check if principal is authenticated. added doubts.md

---
+++ doubts.md
@@ -0,0 +1,5 @@
+if a for a chat application, there is a huge amount of data (message)
+can we just store very old messages in some other dumb and reduce the load on main instances ? 
+- only when user requests for a backup, we need to get all of them ? 
+- what GCP service can be used here ? 
+
--- src/main/java/com/gowthamsagar/springchat/controller/HomeController.java
+++ src/main/java/com/gowthamsagar/springchat/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.gowthamsagar.springchat.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +12,31 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
+
+        String userId = null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // todo: decide which one to use
+        // if (authentication != null && authentication.isAuthenticated()) {}
+        if (authentication != null && authentication.getPrincipal() != null) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof OAuth2User) {
+                OAuth2User oAuth2User = (OAuth2User) principal;
+                // handle userId
+                // todo: what permissions do i need to ask, for username details ?
+                // store github username. not the mail.
+            } else if (principal instanceof UserDetails) {
+                UserDetails userDetails = (UserDetails) principal;
+                userId = userDetails.getUsername();
+            }
+        } else {
+            return "index";
+        }
+
+        if (userId != null) {
+            // get inbox
+            // get messages
+        }
+
         return "index";
     }
 
